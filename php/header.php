51768e3341b0319fb5c9df29db79006dc7530e65
smartcraze <smartcraze354@gmail.com>
2025-04-02 22:32:43 +0530

    adding php

---
+++ php/header.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>Indian Culture</title>
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    
+    <!-- Stylesheets -->
+    <link rel="stylesheet" href="css/font-awesome.min.css">
+    <link rel="stylesheet" href="../css/style.css">
+    <link rel="stylesheet" href="css/owl.carousel.min.css">
+    <link rel="stylesheet" href="css/owl.theme.default.min.css">
+
+    <!-- Favicon -->
+    <link rel="icon" type="image/png" href="https://upload.wikimedia.org/wikipedia/en/thumb/4/41/Flag_of_India.svg/800px-Flag_of_India.svg.png?20111003033457">
+
+    <!-- Google Fonts -->
+    <link href="https://fonts.googleapis.com/css?family=Raleway:300,400,500,600,600i,700" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Montserrat:400,500,500i,600,600i" rel="stylesheet">
+</head>
+<body>
+
+    <nav>
+        <div class="container">
+            <div class="menu-par">
+                <div class="logo-par">
+                    <a href="index.php">
+                        <h2>!NDIA</h2>
+                    </a>
+                </div>
+                <div class="nav">
+                    <ul>
+                        <li><a class="menu_hover" href="index.php">Home</a></li>
+                        <li><a class="menu_hover" href="#slides_parent">Heritage Sites</a></li>
+                        <li><a class="menu_hover" href="#explore-fiji">Travel With Us</a></li>
+                        <li><a class="menu_hover" href="#gallery">Gallery</a></li>
+                        <li><a class="menu_hover" href="#fiji-holyday">Our Pride</a></li>
+                        <li><a class="menu_hover" href="#blog">Blog</a></li>
+                    </ul>
+                </div>
+                <div class="toggle-btn">
+                    <i class="fa fa-bars"></i>
+                </div>
+            </div>
+        </div>
+    </nav>
+
+</body>
+</html>
